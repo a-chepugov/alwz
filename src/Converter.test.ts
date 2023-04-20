26f7d6866e25f18a6924f31b649f00224bd0820e
a.chepugov <a.chepugov@gmail.com>
2023-04-20 19:23:10 +0300

    chore(Converter) test

---
+++ src/Converter.test.ts
@@ -0,0 +1,61 @@
+import assert from 'assert';
+import Converter from './Converter';
+
+describe('Converter', () => {
+	test('sdf', () => {
+		assert(true);
+	})
+	const converter = new Converter((input) => typeof input === 'number' && input > 0, () => 1);
+	converter
+		.undefined(() => 1)
+		.number((i) => {
+			const result = Math.abs(i);
+			return result > 0 ? result : converter.fallback();
+		})
+		.bigint((i) => converter.convert(Number(i)))
+		.string((i) => converter.convert(Number(i)))
+		.symbol((i) => converter.convert(Symbol.keyFor(i)))
+		.register((i) => i instanceof Number, (i: any) => converter.convert(+i));
+
+
+	test(`'is' must be a function`, () => {
+		assert.throws(() => new Converter());
+	});
+
+	test(`'fallback' must be a function`, () => {
+		assert.throws(() => new Converter(() => false));
+	});
+
+	test(`'is' checks if a conversion is needed`, () => {
+		let count = 0;
+		const odd = new Converter((input) => Boolean(input % 2), () => {
+			count++;
+		});
+
+		odd.convert(1);
+		assert.deepEqual(count, 0);
+		odd.convert(2);
+		assert.deepEqual(count, 1);
+	});
+
+	test(`fallback is called when all attempts of conversions had failed`, () => {
+		let count = 0;
+		const odd = new Converter((input) => Boolean(input % 2), () => {
+			count++;
+			return 1;
+		})
+			.undefined(() => 0)
+			.string(() => '3')
+			.register(Array.isArray, () => 5)
+		;
+
+		assert.deepEqual(count, 0);
+		odd.convert(1);
+		odd.convert('2');
+		odd.convert();
+		odd.convert([]);
+		odd.convert(2);
+		assert.deepEqual(count, 1);
+	});
+
+});
--- src/Converter.ts
+++ src/Converter.ts
@@ -1,25 +1,52 @@
-type Convert<I, O> = (input: I) => O;
-type IS = (input: any) => boolean;
+import EV from './Error';
+
+export class InvalidCheckFunction extends EV {};
+export class InvalidFallbackFunction extends EV {};
+export class InvalidConvertFunction extends EV {};
+export class InvalidConverter extends EV {};
+
+type Convert<INPUT, OUTPUT> = (input: INPUT) => OUTPUT;
+type IS<T> = (input: any) => boolean;
 
 /**
  * @description Converts input data to specific type
  */
-export class Convertor<T> {
-	private primitives: Map<string, Convert<any, T>>;
-	private converters: Map<IS, Convert<any, T>>;
-	private readonly is: IS;
-	readonly fallback: () => T;
+export class Converter<T> {
+	is: IS<T>;
+	fallback: () => T;
+
+	#types: Record<string, Convert<any, T>>;
+	#converters: Map<IS<any>, Convert<any, T>>;
 
 	/**
 	 * @param {IS} is - input data type checker
 	 * @param {() => T} fallback- default value generator
 	 */
-	constructor(is: IS, fallback: () => T) {
-		this.is = is;
-		this.primitives = new Map();
-		this.converters = new Map();
-		this.fallback = fallback;
+	constructor(is: IS<T>, fallback: () => T) {
+		if (typeof is === 'function') {
+			this.is = is;
+		} else {
+			throw new InvalidCheckFunction('first argument must be a function', is);
+		}
+
+		if (typeof fallback  === 'function') {
+			this.fallback = fallback;
+		} else {
+			throw new InvalidFallbackFunction('second argument must be a function', fallback);
+		}
+
+		this.#types = {};
+		this.#converters = new Map();
+
 		Object.freeze(this);
+	}
+
+	get types() {
+		return Object.entries(this.#types);
+	}
+
+	get converters() {
+		return Array.from(this.#converters);
 	}
 
 	/**
@@ -27,23 +54,19 @@ export class Convertor<T> {
 	 * @param {any} i - input data
 	 */
 	convert = (i: any): T => {
-		if (this.is(i)) {
-			return i;
-		}
+		if (this.is(i)) return i;
+
 		const type = typeof i;
-		let converter: Convert<any, T> | undefined;
-		if (this.primitives.has(type)) {
-			converter = this.primitives.get(type);
+		if (type in this.#types) {
+			const convert = this.#types[type] as Convert<any, T>;
+			return convert(i);
 		} else {
-			for (const IS of this.converters.keys()) {
-				if (IS(i)) {
-					converter = this.converters.get(IS);
-					break;
+			for (const is of this.#converters.keys()) {
+				if (is(i)) {
+					const convert = this.#converters.get(is) as Convert<any, T>;
+					return convert(i);
 				}
 			}
-		}
-		if (converter) {
-			return converter(i);
 		}
 		return this.fallback();
 	}
@@ -51,10 +74,17 @@ export class Convertor<T> {
 	/**
 	 * @description add `type checker` & `conversion rule` pair into conversions set
 	 * @param {IS} is - input data type checker
-	 * @param {Convertor<any, T>} converter - conversion rule
+	 * @param {Converter<any, T>} converter - conversion rule
 	 */
-	register(is: IS, converter: Convert<any, T>) {
-		this.converters.set(is, converter);
+	register<INPUT>(is: IS<INPUT>, convert: Convert<INPUT, T>) {
+		if (typeof is !== 'function') {
+			throw new InvalidCheckFunction('first argument must be a function', is);
+		}
+		if (typeof convert !== 'function') {
+			throw new InvalidConvertFunction('second argument must be a function', convert);
+		}
+
+		this.#converters.set(is, convert);
 		return this;
 	}
 
@@ -62,58 +92,66 @@ export class Convertor<T> {
 	 * @description del `type checker` & `conversion rule` pair from conversions set
 	 * @param {IS} is - input data type checker
 	 */
-	unregister(IS: IS) {
-		this.converters.delete(IS);
+	unregister<INPUT>(is: IS<INPUT>) {
+		this.#converters.delete(is);
 		return this;
 	}
 
 	/**
 	 * @description conversion rule for `undefined`
 	 */
-	undefined(convertor: Convert<undefined, T>) {
-		this.primitives.set('undefined', convertor);
+	undefined(convert: Convert<undefined, T>) {
+		this.#types.undefined = convert;
 		return this;
 	}
 
 	/**
 	 * @description conversion rule for `boolean`
 	 */
-	boolean(convertor: Convert<boolean, T>) {
-		this.primitives.set('boolean', convertor);
+	boolean(convert: Convert<boolean, T>) {
+		this.#types.boolean = convert;
 		return this;
 	}
 
 	/**
 	 * @description conversion rule for `number`
 	 */
-	number(convertor: Convert<number, T>) {
-		this.primitives.set('number', convertor);
+	number(convert: Convert<number, T>) {
+		this.#types.number = convert;
 		return this;
 	}
 
 	/**
 	 * @description conversion rule for `bigint`
 	 */
-	bigint(convertor: Convert<bigint, T>) {
-		this.primitives.set('bigint', convertor);
+	bigint(convert: Convert<bigint, T>) {
+		this.#types.bigint = convert;
 		return this;
 	}
 
 	/**
 	 * @description conversion rule for `string`
 	 */
-	string(convertor: Convert<string, T>) {
-		this.primitives.set('string', convertor);
+	string(convert: Convert<string, T>) {
+		this.#types.string = convert;
 		return this;
 	}
 
 	/**
 	 * @description conversion rule for `symbol`
 	 */
-	symbol(convertor: Convert<symbol, T>) {
-		this.primitives.set('symbol', convertor);
+	symbol(convert: Convert<symbol, T>) {
+		this.#types.symbol = convert;
 		return this;
+	}
+
+	static assert(instance: any) {
+		if (instance instanceof Converter) {
+			return true;
+		} else {
+			throw new InvalidConverter('Is not an instance of Converter', instance);
+		}
 	}
 }
 
-export default Convertor;
+export default Converter;
