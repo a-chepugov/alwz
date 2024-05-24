6c2e9fad3c9a25449e848670befac8c768d0f546
a.chepugov <a.chepugov@gmail.com>
2024-05-24 19:41:34 +0300

    chore: Error - update signature

--- src/models/Error.ts
+++ src/models/Error.ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line require-jsdoc
 export class ErrorValue extends Error {
-	value: any;
-	constructor(message: string, value: any) {
+	value?: unknown;
+	constructor(message: string, value?: unknown) {
 		super(message);
 		this.value = value;
 		Object.freeze(this);
