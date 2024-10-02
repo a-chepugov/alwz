1a7823c5a2920219a868b5a0cd439387ba2b5652
a.chepugov <a.chepugov@gmail.com>
2024-10-02 14:03:43 +0300

    docs: utils - add `projection` method info

--- src/utils.ts
+++ src/utils.ts
@@ -296,6 +296,41 @@ type ProjectionResult<C, S, O> = {
 	[Key in keyof ProjectionSchema<C, S, O>]: unknown | ProjectionResult<C, S, O>
 }
 
+/**
+ * @description project data into object according to schema
+ * @param {Schema} schema
+ * @returns {Function}
+ * @example
+ * const schema = {
+ *   // shallow element
+ *   a: (source) => source.x + 1,
+ *   // nested schema
+ *   b: {
+ *     c: (source) => source.x + 2,
+ *   },
+ *   // options ( second argument )
+ *   d: (source, options) => options,
+ *   // mid-process result access
+ *   e: (source, options, target) => { target._e = source.x + 3; },
+ *   // call context
+ *   f: function() { return this; },
+ * };
+ *
+ * const project = projection(schema);
+ * const reshape = project(schema);
+ * const source = { x: 1 };
+ * const options = { z: 5 };
+ * const context = { y: 11 };
+ *
+ * project.call(context, source, options);
+ * {
+ *   a: 2,
+ *   b: { c: 3 },
+ *   d: { z: 5 },
+ *   _e: 7, e: undefined,
+ *   f: { y: 11 },
+ * }
+ */
 export const projection = <C, S, O>(
 	schema: ProjectionSchema<C, S, O>
 ): ProjectionBuild<C, S, O> => {
