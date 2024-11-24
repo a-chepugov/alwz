26232e2e893dd67344f8746a806cca144d6ce6dd
a.chepugov <a.chepugov@gmail.com>
2024-11-24 19:27:18 +0200

    docs: Error - add annotaion

--- src/models/ErrorValue.ts
+++ src/models/ErrorValue.ts
@@ -1,12 +1,24 @@
-// eslint-disable-next-line require-jsdoc
+/**
+ * @description Error with value property
+ * @property {any} [value] - can be used to store additional cause info
+ */
 export class ErrorValue extends Error {
 	value?: unknown;
+
+	/**
+	 * @param {string} [message]
+	 * @param {any} [value]
+	 * @param {any} [options]
+	 */
 	constructor(message?: string, value?: unknown, options?: unknown) {
 		// @ts-ignore
 		super(message, options);
 		this.value = value;
 	}
 
+	/**
+	 * @description throw this instance of error
+	 */
 	throw() {
 		throw this;
 	}
